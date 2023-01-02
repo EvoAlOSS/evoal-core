0389aebe49ca4c75acacacb4150fb98be44e2ebe
Bernhard J. Berger <bernhard.berger@uni-bremen.de>
2023-01-02 20:37:15 +0100

    Added real test cases.

--- src/core/de.evoal.core/src/test/java/de/evoal/core/api/board/BlackboardEntryTest.java
+++ src/core/de.evoal.core/src/test/java/de/evoal/core/api/board/BlackboardEntryTest.java
@@ -1,10 +1,20 @@
 package de.evoal.core.api.board;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BlackboardEntryTest {
     @Test
-    public void testEquals() {
+    public void testIsSameToItself() {
+        final BlackboardEntry e1 = BlackboardEntry.of("example");
 
+        Assertions.assertTrue(e1.isSame("example"));
+    }
+
+    @Test
+    public void testIsSameWithDifferentLabel() {
+        final BlackboardEntry e1 = BlackboardEntry.of("example");
+
+        Assertions.assertFalse(e1.isSame("example2"));
     }
 }
