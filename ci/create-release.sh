b0bcb4dd40b79334daaa5f55cd35ded40ed39817
Bernhard J. Berger <bernhard.berger@uni-bremen.de>
2023-01-16 08:31:03 +0100

    Add density to release

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-RELEASE_PLUGINS="generator.main surrogate.api surrogate.simple surrogate.svr"
+RELEASE_PLUGINS="generator.main surrogate.api surrogate.simple surrogate.svr approximative.density"
 
 
 mkdir -p evoal/plugins
