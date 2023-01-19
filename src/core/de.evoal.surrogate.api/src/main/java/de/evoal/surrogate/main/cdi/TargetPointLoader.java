3d478af3d1e64123c1f01d7b6678db275c2d5f77
Bernhard J. Berger <bernhard.berger@uni-bremen.de>
2023-01-19 18:49:58 +0100

    Added watchdog.

--- src/core/de.evoal.surrogate.api/src/main/java/de/evoal/surrogate/main/cdi/TargetPointLoader.java
+++ src/core/de.evoal.surrogate.api/src/main/java/de/evoal/surrogate/main/cdi/TargetPointLoader.java
@@ -52,9 +52,9 @@ public class TargetPointLoader {
             throw new IllegalArgumentException("Unable to read target points file: " + inputFile);
         }
 
-        // TODO FIXME PROPERTIES
         loadedProperties = new PropertiesBasedPropertiesStreamSupplier(new FileBasedPropertiesStreamSupplier(inputFile, PropertiesSpecification.builder().build()).get().collect(Collectors.toList()));
         log.info("Loaded {} target points from '{}'", loadedProperties.size(), inputFile);
+        throw new IllegalStateException("Not yet implemented");        // TODO FIXME PROPERTIES
     }
 
     @Produces @Named("target-stream")
