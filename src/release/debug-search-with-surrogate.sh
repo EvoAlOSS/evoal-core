d88d0cfc055f7936ac2cbaa0f9577e05d5a9c559
Bernhard J. Berger <bernhard.berger@uni-bremen.de>
2023-01-19 01:10:06 +0100

    Fixed execution scripts.

--- src/release/debug-search-with-surrogate.sh
+++ src/release/debug-search-with-surrogate.sh
@@ -17,6 +17,7 @@ java -Dorg.jboss.logging.provider=slf4j\
      --module-path "${EVOALPATH}/modules/:$PLUGIN_PATHS" \
      --add-modules ALL-MODULE-PATH \
      --add-opens java.base/java.lang=guice \
+     --add-exports io.jenetics.base/io.jenetics.internal.math=de.evoal.core \
      --add-exports io.jenetics.base/io.jenetics.internal.collection=de.evoal.core \
      --add-exports io.jenetics.base/io.jenetics.internal.util=de.evoal.core \
      -m de.evoal.core/de.evoal.core.main.Evoal \
