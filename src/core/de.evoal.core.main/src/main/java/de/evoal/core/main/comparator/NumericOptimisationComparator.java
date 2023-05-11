912cb52e4367df3c11af9bfd00a7409be7ebfb1b
Bernhard J. Berger <bernhard.berger@uni-bremen.de>
2023-05-11 08:30:25 +0200

    Added numeric comparator from main branch for testing..

---
+++ src/core/de.evoal.core.main/src/main/java/de/evoal/core/main/comparator/NumericOptimisationComparator.java
@@ -0,0 +1,22 @@
+package de.evoal.core.main.comparator;
+
+import de.evoal.core.api.optimisation.OptimisationValue;
+import de.evoal.core.api.optimisation.OptimisationValueComparator;
+import de.evoal.languages.model.base.Instance;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Named;
+
+@Dependent
+@Named("numeric-comparator")
+public class NumericOptimisationComparator implements OptimisationValueComparator {
+    @Override
+    public OptimisationValue toValue(final double[] fitnessValues) {
+        return NumericOptimisationValue.of(fitnessValues);
+    }
+
+    @Override
+    public OptimisationValueComparator init(final Instance config) {
+        return this;
+    }
+}
