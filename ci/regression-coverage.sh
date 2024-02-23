59743ad01e57b9dd4ba21f6d8e44a10a4815b92d
Bernhard J. Berger <bernhard.berger@tuhh.de>
2024-02-23 11:20:01 +0100

    Profile regression tests.

---
+++ ci/regression-coverage.sh
@@ -0,0 +1,28 @@
+JACOCO_PATH=/Users/berber/bin/jacoco
+
+echo "Downloading Jacoco-Agent"
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -Dmaven.repo.local=${CI_PROJECT_DIR}/.m2/repository -Dartifact=org.jacoco:org.jacoco.agent:0.8.10 -DrepoUrl=https://repo.maven.apache.org/maven2/
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -Dmaven.repo.local=${CI_PROJECT_DIR}/.m2/repository -Dartifact=org.jacoco:org.jacoco.cli:0.8.10 -DrepoUrl=https://repo.maven.apache.org/maven2/
+
+pushd evoal/examples
+
+export EVOAL_VM=-javaagent:${CI_PROJECT_DIR}/.m2/repository/org/jacoco/org.jacoco.agent/0.8.10/org.jacoco.agent-0.8.10-runtime.jar=dumponexit=true
+
+for EXAMPLE in *; do
+    if [ ! -d $EXAMPLE ]; then
+      continue
+    fi 
+
+    pushd $EXAMPLE
+    for SCRIPT in *sh; do
+        echo "Executing example: $SCRIPT"
+        $SH ./$SCRIPT
+    done
+    popd
+
+done
+
+EXECS=$( find . -name "*exec")
+java -jar $JACOCO_PATH/lib/jacococli.jar merge $EXECS --destfile output.execb
+
+popd
