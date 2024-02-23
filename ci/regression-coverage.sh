4b9839a4bfc5b99b07059048b4b6b3d2a572c8cf
Bernhard J. Berger <bernhard.berger@tuhh.de>
2024-02-23 14:38:38 +0100

    Profiling update

--- ci/regression-coverage.sh
+++ ci/regression-coverage.sh
@@ -2,12 +2,12 @@ JACOCO_PATH=/Users/berber/bin/jacoco
 
 echo "Downloading Jacoco-Agent"
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -Dmaven.repo.local=${CI_PROJECT_DIR}/.m2/repository -Dartifact=org.jacoco:org.jacoco.agent:0.8.10 -DrepoUrl=https://repo.maven.apache.org/maven2/
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -Dmaven.repo.local=${CI_PROJECT_DIR}/.m2/repository -Dartifact=org.jacoco:org.jacoco.cli:0.8.10 -DrepoUrl=https://repo.maven.apache.org/maven2/
 
 pushd evoal/examples
 
 export EVOAL_VM=-javaagent:${CI_PROJECT_DIR}/.m2/repository/org/jacoco/org.jacoco.agent/0.8.10/org.jacoco.agent-0.8.10-runtime.jar=dumponexit=true
 
+set -e
 for EXAMPLE in *; do
     if [ ! -d $EXAMPLE ]; then
       continue
@@ -21,8 +21,4 @@ for EXAMPLE in *; do
     popd
 
 done
-
-EXECS=$( find . -name "*exec")
-java -jar $JACOCO_PATH/lib/jacococli.jar merge $EXECS --destfile output.execb
-
 popd
