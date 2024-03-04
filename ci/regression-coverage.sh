775acc649b50c4fd1a94c5a8c6ddc84dec429de5
Bernhard J. Berger <bernhard.berger@tuhh.de>
2024-03-04 13:56:33 +0100

    Hopfully a fix for #52.

--- ci/regression-coverage.sh
+++ ci/regression-coverage.sh
@@ -1,8 +1,9 @@
 JACOCO_PATH=/Users/berber/bin/jacoco
 
 echo "Downloading Jacoco-Agent"
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -Dmaven.repo.local=${CI_PROJECT_DIR}/.m2/repository -Dartifact=org.jacoco:org.jacoco.agent:0.8.10 -DrepoUrl=https://repo.maven.apache.org/maven2/
-
+#mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -Dmaven.repo.local=${CI_PROJECT_DIR}/.m2/repository -Dartifact=org.jacoco:org.jacoco.agent:0.8.10 -DrepoUrl=https://repo.maven.apache.org/maven2/
+mkdir -p ${CI_PROJECT_DIR}/.m2/repository/org/jacoco/org.jacoco.agent/0.8.10/
+curl -o ${CI_PROJECT_DIR}/.m2/repository/org/jacoco/org.jacoco.agent/0.8.10/org.jacoco.agent-0.8.10-runtime.jar https://repo1.maven.org/maven2/org/jacoco/org.jacoco.agent/0.8.10/org.jacoco.agent-0.8.10-runtime.jar
 pushd evoal/examples
 
 export EVOAL_VM=-javaagent:${CI_PROJECT_DIR}/.m2/repository/org/jacoco/org.jacoco.agent/0.8.10/org.jacoco.agent-0.8.10-runtime.jar=dumponexit=true
