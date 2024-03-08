5803a7882b490a279737beecc087b346204f98cb
Bernhard J. Berger <bernhard.berger@tuhh.de>
2024-03-08 14:29:39 +0100

    Updated ConstantExpressionEvaluator to handle InstanceTypeReference #53

--- src/languages/de.evoal.languages.model.interpreter/src/de/evoal/languages/model/interpreter/ConstantExpressionEvaluator.java
+++ src/languages/de.evoal.languages.model.interpreter/src/de/evoal/languages/model/interpreter/ConstantExpressionEvaluator.java
@@ -240,6 +240,8 @@ public class ConstantExpressionEvaluator extends BaseSwitch<Object> {
 			return ((de.evoal.languages.model.ddl.DataReference)object).getDefinition();
 		} else if(object instanceof de.evoal.languages.model.instance.DataReference) {
 			return ((de.evoal.languages.model.instance.DataReference)object).getDefinition();
+		} else if(object instanceof de.evoal.languages.model.base.InstanceDefinitionReference) {
+			return ((de.evoal.languages.model.base.InstanceDefinitionReference)object).getDefinition();
 		}
 		throw new IllegalStateException("Not yet implemented: " + object.eClass() + " -- " + object);			
 	}
