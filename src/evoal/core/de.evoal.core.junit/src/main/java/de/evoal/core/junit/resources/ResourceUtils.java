ba032c2603739c233221299051b0691b0ecb4617
Bernhard Johannes Berger <bernhard.berger@uni-bremen.de>
2024-09-09 23:47:23 +0200

    "'posix:permissions' not supported in Windows"

--- src/evoal/core/de.evoal.core.junit/src/main/java/de/evoal/core/junit/resources/ResourceUtils.java
+++ src/evoal/core/de.evoal.core.junit/src/main/java/de/evoal/core/junit/resources/ResourceUtils.java
@@ -1,5 +1,6 @@
 package de.evoal.core.junit.resources;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,14 +9,15 @@ import java.io.InputStream;
 import java.nio.file.*;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
-import java.util.Enumeration;
-import java.util.HashSet;
-import java.util.Set;
+import java.nio.file.attribute.*;
+import java.util.*;
 
+@Slf4j
 public final class ResourceUtils {
-    private static final Logger log = LoggerFactory.getLogger(ResourceUtils.class);
+    private static final boolean isPosix = FileSystems.getDefault()
+                                                      .supportedFileAttributeViews()
+                                                      .contains("posix");
+
     public static void unpack(final ClassLoader loader, final File destination, final String ... files) throws IOException {
         for(final String base : files) {
             log.info("Searching for file {}.", base);
@@ -24,13 +26,10 @@ public final class ResourceUtils {
                 final URL resource = resources.nextElement();
 
                 final Path outputPath = Path.of(destination.toString(), base);
-                final Set<PosixFilePermission> permissions = new HashSet<>();
-                permissions.add(PosixFilePermission.OWNER_READ);
-                permissions.add(PosixFilePermission.OWNER_WRITE);
-                permissions.add(PosixFilePermission.OWNER_EXECUTE);
 
                 try {
-                    Files.createDirectory(outputPath.getParent(), PosixFilePermissions.asFileAttribute(permissions));
+                    FileAttribute<?> attributes = createFileAttributes();
+                    Files.createDirectory(outputPath.getParent(), attributes);
                 } catch(final FileAlreadyExistsException e) {
                     // ignore
                 }
@@ -42,5 +41,61 @@ public final class ResourceUtils {
                 }
             }
         }
+    }
+
+    private static FileAttribute<?> createFileAttributes() {
+        return isPosix ? createPosixFileAttributes() : createACLFileAttributes();
+    }
+
+    private static FileAttribute<?> createACLFileAttributes() {
+        return new FileAttribute<List<AclEntry>>() {
+
+            @Override
+            public List<AclEntry> value() {
+                // lookup user principal
+                FileSystem fileSystem = FileSystems.getDefault();
+                UserPrincipalLookupService userPrincipalLookupService = fileSystem.getUserPrincipalLookupService();
+                UserPrincipal userPrincipal = null;
+                try {
+                    String username = System.getProperty("user.name");
+                    userPrincipal = userPrincipalLookupService.lookupPrincipalByName(username);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // select ACL flags
+                Set<AclEntryFlag> flags = EnumSet.of(AclEntryFlag.FILE_INHERIT, AclEntryFlag.DIRECTORY_INHERIT);
+
+                // select ACL permission
+                Set<AclEntryPermission> permissions = EnumSet.of(AclEntryPermission.READ_DATA, AclEntryPermission.WRITE_DATA, AclEntryPermission.EXECUTE);
+
+                // build ACL entry
+                AclEntry.Builder builder = AclEntry.newBuilder();
+                builder.setFlags(flags);
+                builder.setPermissions(permissions);
+                builder.setPrincipal(userPrincipal);
+                builder.setType(AclEntryType.DENY);
+
+                AclEntry entry = builder.build();
+                List<AclEntry> aclEntryList = new ArrayList<>();
+                aclEntryList.add(entry);
+
+                return aclEntryList;
+            }
+
+            @Override
+            public String name() {
+                return "acl:acl";
+            }
+        };
+    }
+
+    private static FileAttribute<?> createPosixFileAttributes() {
+        final Set<PosixFilePermission> permissions = new HashSet<>();
+        permissions.add(PosixFilePermission.OWNER_READ);
+        permissions.add(PosixFilePermission.OWNER_WRITE);
+        permissions.add(PosixFilePermission.OWNER_EXECUTE);
+
+        return PosixFilePermissions.asFileAttribute(permissions);
     }
 }
