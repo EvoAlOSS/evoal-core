9851102f12abf174eff8c38edc79b037f22de588
Bernhard Johannes Berger <bernhard.berger@uni-bremen.de>
2025-04-06 11:57:30 +0200

    Introduced a new release script for platform-specific releases and moved it to an earlier stage in the release process.

---
+++ ci/create-and-upload-release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+PLATFORM=$1
+ARCHIVE=evoal-${CI_COMMIT_TAG}-${PLATFORM}.zip
+
+if [[ "$PLATFORM" != "linux-x86_64" ]]; then
+  find evoal -name "*linux-x86_64*jar" | xargs rm
+fi 
+
+if [[ "$PLATFORM" != "macosx-arm64" ]]; then
+  find evoal -name "*macosx-arm64*jar" | xargs rm
+fi 
+
+if [[ "$PLATFORM" != "macosx-x86_64" ]]; then
+  find evoal -name "*macosx-x86_64*jar" | xargs rm
+fi 
+
+if [[ "$PLATFORM" != "windows-x86_64" ]]; then
+  find evoal -name "*windows-x86_64*jar" | xargs rm
+fi 
+
+mv evoal evoal-$PLATFORM
+zip -r $ARCHIVE.zip evoal-$PLATFORM
+curl --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file $ARCHIVE "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/releases/${CI_COMMIT_TAG}/$ARCHIVE"
